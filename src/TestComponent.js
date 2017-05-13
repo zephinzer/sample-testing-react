f37074bc0d0dc448836adecdab274cf809cb7f7a
zephinzer <dev@joeir.net>
2017-05-13 13:05:27 +0800

    adding in props usage example

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -2,10 +2,26 @@ import React, {Component} from 'react';
 import TestComponentChild from './TestComponentChild';
 
 export default class TestComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.getChildComponent = this.getChildComponent.bind(this);
+    this.state = {
+      randomPropValue: Math.random() * 100,
+    };
+  }
+
+  getChildComponent() {
+    return (
+      <TestComponentChild
+        randomProp={this.state.randomPropValue}
+      />
+    );
+  }
+
   render() {
     return (
       <div className="test-component">
-        <TestComponentChild />
+        {this.getChildComponent()}
       </div>
     );
   }
