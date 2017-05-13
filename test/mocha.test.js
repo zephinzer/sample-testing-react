1ae56bf003b833597c8473051142eb1128db1f2f
zephinzer <dev@joeir.net>
2017-05-13 13:06:08 +0800

    refactoring for brevity

--- test/mocha.test.js
+++ test/mocha.test.js
@@ -13,14 +13,10 @@ describe('mocha', () => {
   context('has a context and...', () => {
     beforeEach(() => { currentEnv = Object.assign({}, envA); });
     afterEach(() => { currentEnv = null; });
-    it('should work', () => {
-      expect(currentEnv.a).to.equal(1);
-    });
+    it('should work', () => { expect(currentEnv.a).to.equal(1); });
   });
   context('can have another context and...', () => {
     beforeEach(() => { currentEnv = Object.assign({}, envB); });
-    it('should still work', () => {
-      expect(currentEnv.a).to.equal(2);
-    });
+    it('should still work', () => { expect(currentEnv.a).to.equal(2); });
   });
 });
