94c33b05993f9a74817351bdd27c7e0f70b1fb95
zephinzer <dev@joeir.net>
2017-05-13 13:05:41 +0800

    adding utility code for Function Level testing

---
+++ src/utility/helpers.js
@@ -0,0 +1,9 @@
+export default {
+  summation: (a, b) => a + b,
+  division: (a, b) => a / b,
+  multiplication: (a, b) => a * b,
+  exponent: (a, b) => Math.pow(a, b),
+  subtraction: (a, b) => a - b,
+  bitwiseLeft: (a, b) => a >> b,
+  bitwiseRight: (a, b) => a << b,
+};
